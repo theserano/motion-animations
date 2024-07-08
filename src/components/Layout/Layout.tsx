847d9348f075aa85b2e6ad0c01bab0f011d418bf
Zoe Serano <edwardserano@gmail.com>
2024-07-08 12:54:38 +0100

    learning complete

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,6 +5,11 @@ import "./layout.scss";
 
 const Layout = () => {
 
+  const characters = "Serano".split("");
+  const totalCharacters = characters.length;
+  const animationDuration = 1 * totalCharacters;
+
+
   return (
     <motion.div className="layout">
       <motion.div className="box">
@@ -46,7 +51,7 @@ const Layout = () => {
           className="square"
           animate={{
             scaleY: [1, 1.2, 1.3, 1.1, 1],
-            scaleX: [1, 1.1, 1.3, 1.1, 1],
+            scaleX: [1, 1.2, 1.3, 1.1, 1],
             borderRadius: ["0px", "5px", "15px", "10px", "0px"],
           }}
           transition={{
@@ -62,16 +67,92 @@ const Layout = () => {
       <motion.div className="box">
         <motion.div
           className="square"
+          animate={{
+            x: [-100, -70, -50, 10, -50, -70, -100],
+            scale: [0.2, 0.4, 0.6, 1.0, 0.6, 0.2],
+            rotateZ: ["45deg", "180deg", "45deg"],
+            borderRadius: ["0px", "5px", "15px", "10px", "0px"],
+          }}
           transition={{
-            duration: 2,
-            ease: "easeInOut",
-            times: [0, 0.2, 0.5, 0.8, 1],
+            duration: 1,
+            ease: "linear",
+            times: [0, 0.1, 0.3, 0.6, 0.8, 1],
             repeat: Infinity,
             repeatDelay: 1,
           }}
         />
       </motion.div>
-      
+
+      <motion.div
+        className="box"
+        style={{ display: "flex", overflow: "hidden" }}
+      >
+        {characters.map((char, index) => (
+          <motion.span
+            key={index}
+            style={{ display: "inline-block", opacity: 0 }}
+            animate={{
+              opacity: [0, 1, 1, 0],
+              x: ["100%", "0%", "0%", "-50%"],
+            }}
+            transition={{
+              times: [0, 0.25, 0.75, 1],
+              duration: animationDuration,
+              repeat: Infinity,
+              repeatType: "loop",
+              ease: "linear",
+              delay: index++,
+            }}
+          >
+            {char}
+          </motion.span>
+        ))}
+      </motion.div>
+
+      <motion.div className="box">
+        {characters.map((char, index) => (
+          <motion.span
+            key={index}
+            // style={{opacity: 0}}
+            animate={{
+              opacity: [0, 1, 1, 0],
+            }}
+            transition={{
+              times: [0, 0.25, 0.75, 1],
+              duration: 2,
+              repeat: Infinity,
+              repeatType: "loop",
+              ease: "linear",
+              delay: index,
+            }}
+          >
+            {char}
+          </motion.span>
+        ))}
+      </motion.div>
+
+      <motion.div className="box">
+        <motion.span
+          style={{
+            border: "5px solid white",
+            borderStyle: "none solid none none",
+            overflow: "hidden",
+          }}
+          animate={{
+            width: [0, 100, 320, 100, 0],
+          }}
+          transition={{
+            times: [0, 0.25, 0.5, 0.75, 1],
+            duration: 2,
+            repeat: Infinity,
+            repeatType: "loop",
+            ease: "circInOut",
+            repeatDelay: 1,
+          }}
+        >
+          Serano
+        </motion.span>
+      </motion.div>
     </motion.div>
   );
 }
